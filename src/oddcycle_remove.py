1dca8812be3425368ae467f0614b83fbbc7c1bb6
Tomoya Fujii <ftomoya2213@g.ecc.u-tokyo.ac.jp>
2024-12-13 18:00:02 +0900

    added main function

--- src/oddcycle_remove.py
+++ src/oddcycle_remove.py
@@ -1,4 +1,4 @@
-# heuristic to haplotype phasing by removing odd cycles as many as possible
+# heuristic for haplotype phasing by removing odd cycles as many as possible
 
 from open_input_file import read_file
 from build_graph import build_graph
@@ -6,7 +6,24 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-r, s, M = read_file('../data/input_2.txt')
-G = build_graph(r, s, M)
-# nx.draw(G, with_labels=True)
-# plt.show()
+def enumerate_faces(embedding):
+    faces = []
+    for cycle in embedding.faces_iter():
+        faces.append(list(cycle))
+    return faces
+
+def main(path):
+    r, s, M = read_file(path)
+    G = build_graph(r, s, M)
+    is_planar, embedding = nx.check_planarity(G, counterexample=False)
+    if is_planar:
+        print("The graph is planar")
+        faces = enumerate_faces(embedding)
+        for face in faces:
+            print(face)
+    else:
+        print("The graph is not planar")
+    nx.draw(G, with_labels=True)
+    plt.show()
+    
+main('../data/input_1.txt')
