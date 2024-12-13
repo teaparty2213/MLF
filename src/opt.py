88c0fd5660fb5a8ce0dd04b3f81a5a8fc1492087
Tomoya Fujii <ftomoya2213@g.ecc.u-tokyo.ac.jp>
2024-12-13 14:25:51 +0900

    completed coding

--- src/opt.py
+++ src/opt.py
@@ -1,8 +1,14 @@
-# O(2^K*s) time exact algorithm for heterozygous phasing (K: max coverage, s: number of SNP positions)
+##-------------------------------------------------------------
+## Reference: Genome-Scale Algorithm Design 2nd edition, Chapter 14: Haplotype analysis
+## O(2^K*s) time exact algorithm for haplotype phasing (K: max coverage, s: number of SNP positions)
+## Left to do: implement traceback to find the optimal bipartition
+## Special thanks to Tomoyuki Unno for helping implement
+##-------------------------------------------------------------
 
 from open_input_file import read_file
 from gray_code import gray_code, gray_code_dif_bit, get_Nth_bit
 import numpy as np
+import time
 
 def read_range(r, s, M): # calculate the range of read index at each site
     read_range_list = []
@@ -54,7 +60,7 @@ def opt_algo(r, s, M, read_range_list, K): # O(2^K*s) time exact algorithm for h
     last = read_range_list[0][1]
     width = last - first + 1
     next_first = read_range_list[1][0]
-    shared_width = last - next_first + 1
+    next_shared_width = last - next_first + 1
     count = count_allele(M, 0, first, last, 0) # gray_code = 0
     min_flip = min(count[0], count[1]) + min(count[2], count[3])
     for i in range(1, 2 ** width): # i: gray_code
@@ -79,14 +85,65 @@ def opt_algo(r, s, M, read_range_list, K): # O(2^K*s) time exact algorithm for h
                 
         min_flip = min(count[0], count[1]) + min(count[2], count[3])
         D[0][gray] = min_flip
-        shared_idx = (gray >> (width - shared_width)) & ((1 << shared_width) - 1) # digits used in the next site
+        shared_idx = (gray >> (width - next_shared_width)) & ((1 << next_shared_width) - 1) # digits used in the next site
         shared_D[shared_idx] = min(shared_D[shared_idx], D[0][gray])
+    
+    # recursion
+    min_D = r * s
+    for j in range(1, s):
+        first = read_range_list[j][0]
+        last = read_range_list[j][1]
+        width = last - first + 1
+        pre_shared_width = next_shared_width
+        pre_shared_mask = (1 << pre_shared_width) - 1
+        if (j < s - 1):
+            next_first = read_range_list[j + 1][0]
+            next_shared_width = last - next_first + 1
+        count = count_allele(M, j, first, last, 0) # gray_code = 0
+        min_flip = min(count[0], count[1]) + min(count[2], count[3])
+        
+        for i in range(1, 2 ** width): # i: gray_code
+            gray = gray_code(i)
+            pre_gray = gray_code(i - 1)
+            dif_bit = gray_code_dif_bit(gray, pre_gray)
+            
+            if (get_Nth_bit(gray, dif_bit) == 0): # Group of read[first + dif_bit] changes from Y to X
+                if (M[first + dif_bit][j] == 0): # #0 in X increases by 1, #0 in Y decreases by 1
+                    count[0] += 1
+                    count[2] -= 1
+                elif (M[first + dif_bit][j] == 1): # #1 in X increases by 1, #1 in Y decreases by 1
+                    count[1] += 1
+                    count[3] -= 1
+            elif (get_Nth_bit(gray, dif_bit) == 1): # Group if read[first + dif_bit] changes from X to Y
+                if (M[first + dif_bit][j] == 0): # #0 in X decreases by 1, #0 in Y increases by 1
+                    count[0] -= 1
+                    count[2] += 1
+                elif (M[first + dif_bit][j] == 1): # #1 in X decreases by 1, #1 in Y increases by 1
+                    count[1] -= 1
+                    count[3] += 1
+            
+            min_flip = min(count[0], count[1]) + min(count[2], count[3])
+            D[j][gray] = shared_D[gray & pre_shared_mask] + min_flip
+            if (j < s - 1):
+                shared_idx = (gray >> (width - next_shared_width)) & ((1 << next_shared_width) - 1) # digits used in the next site
+                pre_shared_D[shared_idx] = min(pre_shared_D[shared_idx], D[j][gray])
+            else: # j = s - 1 (last column)
+                min_D = min(min_D, D[j][gray])
                 
+        shared_D = pre_shared_D.copy()
+        pre_shared_D = [r * s] * max_states
+    return min_D
 
 def main(path):
     r, s, M = read_file(path)
+    
+    start = time.perf_counter()
     read_range_list = read_range(r, s, M)
     K = max_coverage(read_range_list)
-    opt_algo(r, s, M, read_range_list, K)
+    min_D = opt_algo(r, s, M, read_range_list, K)
+    end = time.perf_counter()
+    
+    print("Minimum Letter Flip: {}".format(min_D))
+    print("Time: {}s".format(end - start))
 
 main('../data/input_1.txt')
